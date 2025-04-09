20f6363085d18bceea8b05c90f540913d7f17b2a
borsodi-koppany <borsodi.koppany@students.jedlik.eu>
2025-04-09 16:59:32 +0200

    inputcheck is done

--- DetatilsWindow.xaml.cs
+++ DetatilsWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace WPF_PROJECT
     public partial class DetatilsWindow : Window
     {
         public Phone onePhone { get; set; }
+
+        
         public DetatilsWindow(Phone phone)
         {
             InitializeComponent();
@@ -29,12 +31,61 @@ namespace WPF_PROJECT
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            if (InputCheck())
+            {
+                DialogResult = true;
+                Close();
+            }
         }
 
         private bool InputCheck()
         {
+            if(String.IsNullOrWhiteSpace(onePhone.Name))
+            {
+                MessageBox.Show("Please, enter the name of the phone", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if(String.IsNullOrWhiteSpace(onePhone.Display.Size.ToString()) || onePhone.Display.Size < 0) {
+                MessageBox.Show("Please, enter a number bigger than 0 for display size!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if(String.IsNullOrWhiteSpace(onePhone.Display.Type))
+            {
+                MessageBox.Show("Please enter the display type!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (String.IsNullOrWhiteSpace(onePhone.Release.ToString()))
+            {
+                MessageBox.Show("Please enter the release date!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if(String.IsNullOrWhiteSpace(onePhone.Battery.ToString()))
+            {
+                MessageBox.Show("Please enter the battery size", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (String.IsNullOrWhiteSpace(onePhone.Rom.Type))
+            {
+                MessageBox.Show("Please enter the ROM type", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if(String.IsNullOrWhiteSpace(onePhone.Rom.Version.ToString()))
+            {
+                MessageBox.Show("Please enter the version of the Rom", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if(String.IsNullOrWhiteSpace(onePhone.Main_camera.ToString()))
+            {
+                MessageBox.Show("Please enter data about the main camera", "Error", MessageBoxButton.OK, MessageBoxImage.Error) ;
+                return false;
+            }
             return true;
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
         }
     }
 }
