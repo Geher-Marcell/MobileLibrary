f695468951ac8cf99f903be3b2f5fea877ea96bd
KissMartin <kiss.martin2@students.jedlik.eu>
2025-04-09 22:36:20 +0200

    Everything besides DetailsWindow button spacing is fixed

--- DetatilsWindow.xaml.cs
+++ DetatilsWindow.xaml.cs
@@ -34,9 +34,55 @@ namespace WPF_PROJECT
             InitializeComponent();
             onePhone = phone;
             DataContext = this;
-            MessageBox.Show(onePhone.MainCamera.ToString());
         }
 
+        private bool InputCheck()
+        {
+            if (!CheckField(onePhone.Name, string.IsNullOrWhiteSpace, PhoneValidationError.NameEmpty)) return false;
+
+            if (!CheckField(onePhone.Display!.Size, val => val <= 0, PhoneValidationError.DisplaySizeInvalid)) return false;
+
+            if (!CheckField(onePhone.Display.Type, string.IsNullOrWhiteSpace, PhoneValidationError.DisplayTypeEmpty)) return false;
+
+            if (!CheckField(onePhone.Release, val => val <= 0, PhoneValidationError.ReleaseDateInvalid)) return false;
+
+            if (!CheckField(onePhone.Battery, val => val <= 0, PhoneValidationError.BatterySizeInvalid)) return false;
+
+            if (!CheckField(onePhone.Rom!.Type, string.IsNullOrWhiteSpace, PhoneValidationError.RomTypeEmpty)) return false;
+
+            if (!CheckField(onePhone.Rom.Version, val => val <= 0, PhoneValidationError.RomVersionInvalid)) return false;
+
+            if (!CheckField(onePhone.MainCamera, val => val <= 0, PhoneValidationError.MainCameraInvalid)) return false;
+
+            return true;
+        }
+
+        private bool CheckField<T>(T value, Func<T, bool> condition, PhoneValidationError errorType)
+        {
+            if (condition(value))
+            {
+                string errorMessage = GetErrorMessage(errorType);
+                MessageBox.Show(errorMessage, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            return true;
+        }
+
+        private string GetErrorMessage(PhoneValidationError error)
+        {
+            return error switch
+            {
+                PhoneValidationError.NameEmpty => "Please, enter the name of the phone",
+                PhoneValidationError.DisplaySizeInvalid => "Please, enter a number bigger than 0 for display size!",
+                PhoneValidationError.DisplayTypeEmpty => "Please enter the display type!",
+                PhoneValidationError.ReleaseDateInvalid => "Please enter the release date!",
+                PhoneValidationError.BatterySizeInvalid => "Please enter the battery size",
+                PhoneValidationError.RomTypeEmpty => "Please enter the ROM type",
+                PhoneValidationError.RomVersionInvalid => "Please enter the version of the Rom",
+                PhoneValidationError.MainCameraInvalid => "Please enter data about the main camera",
+                _ => "Unknown error occurred",
+            };
+        }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             if (InputCheck())
@@ -46,54 +92,25 @@ namespace WPF_PROJECT
             }
         }
 
-        private bool InputCheck()
-        {
-            if(String.IsNullOrWhiteSpace(onePhone.Name))
-            {
-                MessageBox.Show("Please, enter the name of the phone", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                return false;
-            }
-            if(String.IsNullOrWhiteSpace(onePhone.Display.Size.ToString()) || onePhone.Display.Size <= 0) {
-                MessageBox.Show("Please, enter a number bigger than 0 for display size!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                return false;
-            }
-            if(String.IsNullOrWhiteSpace(onePhone.Display.Type))
-            {
-                MessageBox.Show("Please enter the display type!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                return false;
-            }
-            if (String.IsNullOrWhiteSpace(onePhone.Release.ToString()) || onePhone.Release <= 0)
-            {
-                MessageBox.Show("Please enter the release date!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                return false;
-            }
-            if(String.IsNullOrWhiteSpace(onePhone.Battery.ToString()) || onePhone.Battery <= 0)
-            {
-                MessageBox.Show("Please enter the battery size", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                return false;
-            }
-            if (String.IsNullOrWhiteSpace(onePhone.Rom.Type))
-            {
-                MessageBox.Show("Please enter the ROM type", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                return false;
-            }
-            if(String.IsNullOrWhiteSpace(onePhone.Rom.Version.ToString()) || onePhone.Rom.Version <= 0)
-            {
-                MessageBox.Show("Please enter the version of the Rom", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                return false;
-            }
-            if(String.IsNullOrWhiteSpace(onePhone.MainCamera.ToString()) || onePhone.MainCamera <= 0)
-            {
-                MessageBox.Show("Please enter data about the main camera", "Error", MessageBoxButton.OK, MessageBoxImage.Error) ;
-                return false;
-            }
-            return true;
-        }
-
         private void btnCancel_Click(object sender, RoutedEventArgs e)
         {
             DialogResult = false;
             Close();
         }
     }
+
+    public enum PhoneValidationError
+    {
+        NameEmpty,
+        DisplaySizeInvalid,
+        DisplayTypeEmpty,
+        ReleaseDateInvalid,
+        BatterySizeInvalid,
+        RomTypeEmpty,
+        RomVersionInvalid,
+        MainCameraInvalid
+
+
+    }
+
 }
